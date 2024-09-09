f07626141657497c2dd174dd37ec2e40a12df187
Luke-Dev90 <lukee_sf@hotmail.com>
2024-09-09 10:11:31 -0300

    pruebas

---
+++ src/main/java/com/lchalela/maven/db/PruebadbMavenApplication.java
@@ -0,0 +1,14 @@
+package com.lchalela.maven.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebadbMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebadbMavenApplication.class, args);
+		System.out.println("Iniciar");
+	}
+
+}
---
+++ src/test/java/com/lchalela/maven/db/PruebadbMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lchalela.maven.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebadbMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
